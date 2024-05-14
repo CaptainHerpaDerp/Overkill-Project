bc508bc93af9eaf03aa0013fa49dceb8e6fc78f3
Ruben Smith <49171379+CaptainHerpaDerp@users.noreply.github.com>
2024-05-14 12:05:16 +0200

    push

---
+++ Assets/Scripts/BridgeTest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTest : MonoBehaviour
+{
+    [SerializeField] private BoxCollider bridge1Coll, bridge2Coll;
+
+
+}
